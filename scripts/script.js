7a3f9ed397adf149706bce30d2f896f7093de29d
JustinPrins98 <118190801+JustinPrins98@users.noreply.github.com>
2022-12-20 18:03:55 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+
+var menuButton = document.querySelector("header nav button");
+
+menuButton.addEventListener("click", menuOpenen);
+
+function menuOpenen(){
+    var deUl = document.querySelector("ul");
+    deUl.classList.toggle("menuOpen");
+}
